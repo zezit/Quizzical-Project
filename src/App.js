c8524a998bbbb28dffb86a8b6ecf2a3f27facffa
José Victor <victdiasmd@gmail.com>
2022-07-19 17:32:23 -0300

    iniciando separação de arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+// import { CTA, Brand, Navbar } from './components';
+
+import './style/App.css';
+import yellow from "./images/blob 5.png"
+import violet from "./images/blobs.png"
+
+const App = () => (
+  <div className="App">
+    <img className="back--yellow" src={yellow} alt="back blob y"/>
+    <img className="back--violet" src={violet} alt="back blob" v/>
+    App
+    {/* <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer /> */}
+  </div>
+);
 
 export default App;
